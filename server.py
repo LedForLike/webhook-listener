00df341a41b22f18a4a1d3cfdcfce9e2308b0c34
Amir Sasson <Amirsas@gmail.com>
2017-09-11 20:12:25 +0300

    Update server.py

--- server.py
+++ server.py
@@ -22,3 +22,8 @@ def webhook():
     mqttc.publish('topic', content)
     print(content)
     return 'ok'
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
