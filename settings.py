fedaa972e426f406c0ea8357712787da436600ea
Amir Sasson <amirsas@gmail.com>
2017-09-14 19:08:50 +0300

    remove secrets

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 """Application Config"""
 import os
 
-MQTT_HOST = os.environ.get("mqtt-host", 'm21.cloudmqtt.com')
-MQTT_USER = os.environ.get("mqtt-user", 'vhxoajqj')
-MQTT_PWD = os.environ.get("mqtt-pwd", 'K0oIgMwjl93S')
-MQTT_PORT = int(os.environ.get("mqtt-port", 13143))
+MQTT_HOST = os.environ.get("mqtt-host", '')
+MQTT_USER = os.environ.get("mqtt-user", '')
+MQTT_PWD = os.environ.get("mqtt-pwd", '')
+MQTT_PORT = int(os.environ.get("mqtt-port", ))
 MQTT_FB_WEBHOOK_TOPIC_NAME = 'fb-posts-updates'
-WEB_PORT = int(os.environ.get("PORT", 5000))
+WEB_PORT = int(os.environ.get("PORT", ))
 PAGE_ACCESS_TOKEN = os.environ.get("PAGE_ACCESS_TOKEN",
- "EAACEdEose0cBAHCT4rmoZBdDxGB5HxyEOSGwnQkRelZAitLObpZAoZCzhu7imogQg8RGGvYUQv1ahkKQquqLA4gR0ZBZBPbnbiI6zwcwsC9FO8FSoLV44eaqb7pBtPPoQvugkiZAB8DmXeJQEsI1JZCKtK8OQgZAFCaqwCzapN8VZA7AmaISJgUfzjCdqJlxOy1uYZD")
-FB_PAGE_ID = os.environ.get("FB_PAGE_ID", "1948455872076947")
+ "")
+FB_PAGE_ID = os.environ.get("FB_PAGE_ID", "")
 
