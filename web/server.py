6196a763c4c1d4ccfb5e1d52d1c1dcc61f50a860
Amir Sasson <amirsas@gmail.com>
2017-09-14 09:38:13 +0300

    fix

--- web/server.py
+++ web/server.py
@@ -5,29 +5,27 @@ from flask import Flask
 from lib.mqtt_client import MqttClient
 import settings as Config
 
+MQTTC = MqttClient()
+# pylint: disable=no-method-argument
 class Server(object):
     """Server class"""
     app = Flask(__name__)
 
-    def __init__(self):
-        self.mqttc = MqttClient()
-
     @app.route("/")
-    # pylint: disable=no-method-argument
     def hello():
         """default"""
         return "Hello World!"
 
     @app.route("/webhook", methods=['GET'])
-    # pylint: disable=no-method-argument
-    def verify():        
+    def verify():
         """webhook api"""
         return request.args.get('hub.challenge')
-    
+
     @app.route("/webhook", methods=['POST'])
-    def webhook(self):
+    def webhook():
         """webhook api"""
+        logging.debug('Handling webhook request!!')
         content = request.get_json()
-        self.mqttc.publish(Config.MQTT_FB_WEBHOOK_TOPIC_NAME, str(content))
+        MQTTC.publish(Config.MQTT_FB_WEBHOOK_TOPIC_NAME, str(content))
         logging.info('Handled webhook request ' + str(content))
         return ''
